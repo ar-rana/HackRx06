ca1a8a727f38683ab52dada9f1e73b1ecca45e49
Aryan Rana <aryan.rana2005@gmail.com>
2025-08-03 15:27:53 +0530

    added exception handler

---
+++ server/src/main/java/com/project/server/config/ExceptionAdvice.java
@@ -0,0 +1,26 @@
+package com.project.server.config;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ExceptionAdvice {
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<String> handleExpiredJwtException(ExpiredJwtException ex) {
+        log.error("JWT Session expired, msg: {}", ex.getMessage());
+        return ResponseEntity.status((HttpStatus.UNAUTHORIZED)).body("your session has expired");
+    }
+
+    @ExceptionHandler(MalformedJwtException.class)
+    public ResponseEntity<String> handleMalformedJwtException(MalformedJwtException ex) {
+        log.error("JWT token abnormality detected: msg {}", ex.getMessage());
+        return ResponseEntity.status((HttpStatus.CONFLICT)).body("your session has expired");
+    }
+}
